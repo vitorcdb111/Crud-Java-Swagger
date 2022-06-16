cc9e4197812eafb31e98dddec974d6c46072c8ec
Vítor De Bortoli <caetano.vitordb@gmail.com>
2022-06-16 11:29:53 -0300

    adicionando swagger

---
+++ src/main/java/com/trabalho/pooa/character/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.trabalho.pooa.character.config;
+
+import io.swagger.annotations.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.trabalho.pooa.character"))
+                .paths(regex("/api/character.*"))
+                .build();
+    }
+
+}
--- src/main/java/com/trabalho/pooa/character/controller/RpgCharacterController.java
+++ src/main/java/com/trabalho/pooa/character/controller/RpgCharacterController.java
@@ -2,14 +2,17 @@ package com.trabalho.pooa.character.controller;
 
 import com.trabalho.pooa.character.model.RpgCharacter;
 import com.trabalho.pooa.character.repository.RpgCharacterRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/character")
+@Api(value="API REST RPG")
 public class RpgCharacterController {
 
     private final RpgCharacterRepository repository;
@@ -18,22 +21,26 @@ public class RpgCharacterController {
         this.repository = repository;
     }
 
+    @ApiOperation(value="Retorna uma lista de personagens do rpg")
     @GetMapping
     public List<RpgCharacter> findAllCharacters() {
         return repository.findAll();
     }
 
+    @ApiOperation(value="Retorna um personagem pelo id")
     @GetMapping("/{CharacterId}")
     public RpgCharacter findCharacterById(@PathVariable Integer CharacterId) {
         return repository.findById(CharacterId).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @ApiOperation(value="Inserir um personagem passando seus atributos pelo corpo")
     @PostMapping
     public RpgCharacter createNewCharacter(@RequestBody RpgCharacter newCharacter) {
         return repository.save(newCharacter);
     }
 
+    @ApiOperation(value="Atualizar um personagem, passando seu id no endereco")
     @PutMapping
     public RpgCharacter updateCharacter(@RequestBody RpgCharacter CharacterRequest) {
         if(repository.existsById(CharacterRequest.getId())) {
@@ -42,6 +49,7 @@ public class RpgCharacterController {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Character id does not exists.");
     }
 
+    @ApiOperation(value="Deletar um personagem, passando seu id no endereco")
     @DeleteMapping("/{CharacterId}")
     public void deleteMapping(@PathVariable Integer CharacterId) {
         if(repository.existsById(CharacterId)) {
